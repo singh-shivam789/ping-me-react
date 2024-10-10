54fc297883f3c0f8d4a4f6cc0832b916540f15e0
Shivam Singh <shivamsinghgbpit@gmail.com>
2024-10-10 19:13:18 +0530

    Refactor UserInfo component

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import './userInfo.css'
-export default function UserInfo({user}) {
+export default function UserInfo({ user }) {
   return (
-    <div className={user == "chatTop" ? "userInfoChat": "userInfoList"}>
-        <div className={user == "chatTop" ? "userChat": "userList"}>
-            <img src="/avatar.png"></img>
-            <div>
-            {user == "chatTop" && <h4>Akinchand</h4>}
-            {user != "chatTop" && <h2>Akinchand</h2>}
-            {user == "chatTop" && <p className='userStatus'>Lorem ipsum, dolor sit amet consectetur </p>}
-            </div>
+    <div className={user === "chatTop" ? "userInfoChat" : "userInfoList"}>
+      <div className={user === "chatTop" ? "userChat" : "userList"}>
+        <img src="/avatar.png"></img>
+        <div>
+          <>
+            {user === "chatTop" ? (
+              <>
+                <h4>Akinchand</h4>
+                <p className="userStatus">Lorem ipsum, dolor sit amet consectetur</p>
+              </>
+            ) : (
+              <h2>Akinchand</h2>
+            )}
+          </>
+
         </div>
-        <div className={user == "chatTop" ? "iconsChat": "iconsList"}>
-            <img src={user == "chatTop" ? "/phone.png" : "/more.png"}/>
-            <img src="/video.png"/>
-            <img src={user == "chatTop" ? "/info.png" : "/edit.png"}/>
-        </div>
+      </div>
+      <div className={user === "chatTop" ? "iconsChat" : "iconsList"}>
+        <img src={user === "chatTop" ? "/phone.png" : "/more.png"} />
+        <img src="/video.png" />
+        <img src={user === "chatTop" ? "/info.png" : "/edit.png"} />
+      </div>
     </div>
   )
 }
