ca925fa0bbe7d696f257b459cd27c86bf32e44b9
singh-shivam789 <shivamsinghgbpit@gmail.com>
2025-06-21 19:04:06 +0530

    feat(detail): add FriendOptions and SelfOptions components

---
+++ src/components/detail/options/FriendOptions.jsx
@@ -0,0 +1,73 @@
+export default function FriendOptions() {
+  return (
+    <div className="options">
+      <div className="option sharedFiles">
+        <div className="files">
+          <span>Chat Settings</span>
+          <div className="expandBtn">
+            <img src="/arrowUp.png" alt="arrowUp.png" />
+          </div>
+        </div>
+        <div className="filesExpanded">
+        </div>
+      </div>
+      <div className="option sharedFiles">
+        <div className="files">
+          <span>Privacy & Help</span>
+          <div className="expandBtn">
+            <img src="/arrowUp.png" alt="arrowUp.png" />
+          </div>
+        </div>
+        <div className="filesExpanded">
+        </div>
+      </div>
+      <div className="option sharedFiles">
+        <div className="files">
+          <span>Shared Images</span>
+          <div className="expandBtn">
+            <img src="/arrowUp.png" alt="arrowUp.png" />
+          </div>
+        </div>
+        <div className="filesExpanded">
+        </div>
+      </div>
+      <div className="option sharedFiles">
+        <div className="files">
+          <span>Shared Files</span>
+          <div className="expandBtn">
+            <img src="/arrowUp.png" alt="arrowUp.png" />
+          </div>
+        </div>
+        <div className="filesExpanded">
+          <div className="file">
+            <div className="previewImageInfo">
+              <img className="previewImg" src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?cs=srgb&dl=pexels-pixabay-210019.jpg&fm=jpg" alt="img.png" />
+              <span className="fileName">photo_1.png</span>
+            </div>
+            <div className="expandBtn">
+              <img src="download.png" alt="" />
+            </div>
+          </div>
+          <div className="file">
+            <div className="previewImageInfo">
+              <img className="previewImg" src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?cs=srgb&dl=pexels-pixabay-210019.jpg&fm=jpg" alt="img.png" />
+              <span className="fileName">photo_1.png</span>
+            </div>
+            <div className="expandBtn">
+              <img src="download.png" alt="" />
+            </div>
+          </div>
+          <div className="file">
+            <div className="previewImageInfo">
+              <img className="previewImg" src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?cs=srgb&dl=pexels-pixabay-210019.jpg&fm=jpg" alt="img.png" />
+              <span className="fileName">photo_1.png</span>
+            </div>
+            <div className="expandBtn">
+              <img src="download.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
