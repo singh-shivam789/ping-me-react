53ca65fee8bcc04b16455444c55652a02198ce3c
singh-shivam789 <shivamsinghgbpit@gmail.com>
2025-06-21 18:59:57 +0530

    feat(detail): drive Detail view from chatStore and add friend/self option

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,13 +1,24 @@
 import { getInitialUserState, logoutUser } from "../../utils/userUtils";
+import useChatStore from "../../lib/stores/user/chatStore";
 import useUserStore from "../../lib/stores/user/userStore";
 import { getInitialAppState } from "../../utils/appUtils";
 import useAppStore from "../../lib/stores/app/appStore";
+import FriendOptions from "./options/FriendOptions";
+import SelfOptions from "./options/SelfOptions";
 import UserInfo from "../shared//UserInfo";
-import Options from "./options/Options";
 import { toast } from "react-toastify";
 import "./detail.css";
 
+
 export default function Detail() {
+  const chatUser = useChatStore((state) => state.chatUser);
+  const currentUser =  useUserStore((state) => state.user);
+  const removeFriendHandler = () => {
+    //TODO: to implement later
+  }
+  const deleteUserHandler = () => {
+    //TODO: to implement later
+  }
   const handleLogOut = async (e) => {
     try {
       await logoutUser();
@@ -15,6 +26,7 @@ export default function Detail() {
       useUserStore.setState(getInitialUserState());
       useAppStore.persist.clearStorage();
       useAppStore.setState(getInitialAppState());
+      useChatStore.persist.clearStorage();
       toast.dark("Bye!");
     }
     catch (err) {
@@ -24,14 +36,11 @@ export default function Detail() {
   }
   return (
     <div className="detail">
-      {    
-        //useStore user is dummy value, will be added when chatStore will be developed
-      }
-      <UserInfo whichUserPage={"userDetail"} user={useUserStore.getState().user} />
-      <Options />
+      {chatUser && <UserInfo whichUserPage={"userDetail"} user={chatUser} />}
+      {chatUser._id !== currentUser._id ? (<FriendOptions />) : (<SelfOptions />)}
       <div className="logoutAndBlock">
         <div className="blockUser">
-          <button className="blockUserBtn">Block User</button>
+          <button onClick={chatUser._id !== currentUser._id ? removeFriendHandler : deleteUserHandler} className="blockUserBtn">{chatUser._id !== currentUser._id ? "Remove friend" : "Delete Account"}</button>
         </div>
         <div className="logout">
           <button onClick={handleLogOut} className="logoutBtn">Logout</button>
